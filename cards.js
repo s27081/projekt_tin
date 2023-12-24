78b52355ea7ddf3fdbda3e63465d8264e1e924d6
Ziem0 <ziemekorlikowski@gmail.com>
2023-12-24 19:19:25 +0100

    cards logic

--- cards.js
+++ cards.js
@@ -1,14 +1,50 @@
-const testList = {"test" : "test1",
-                  "cos" : "cos2",
-                  "cat":"kot"
+const testList = [["test","test1"],
+                  ["cos","cos2"],
+                  ["cat","kot"]
+]
+
+
+const testList2 = [["dog","pies"],
+                  ["cow","krowa"],
+                ["giraffe","zyrafa"]];
+                
+const lists = [testList, testList2]
+
+
+function saveValuesToFile(selectedList) {
+  localStorage.setItem("selectedList", JSON.stringify(selectedList));
 }
 
 
 let pageDisplay = document.querySelector(".content");
 
-for (let i in testList){
-    let subValue = i + " " + testList[i];
+for (let i=0; i<lists.length; i++){
+    let newContaner = document.createElement("div");
+    newContaner.setAttribute("class","cardContainer");
+    document.addEventListener("click", function(e){
+      const target = e.target.closest(".cardContainer");
+      const selectedElements = document.querySelectorAll(".cardContainer.selected");
+    
+      if (target) {
+        selectedElements.forEach((element) => {
+          element.classList.remove("selected");
+        });
+        target.classList.add("selected");
+
+      const selectedListIndex = Array.from(target.parentNode.children).indexOf(target);
+      const selectedList = lists[selectedListIndex];
+
+        saveValuesToFile(selectedList);
+      }
+})
+  for (let j=0; j<lists[i].length; j++){
+    let subValue = lists[i][j];
     subLi = document.createElement('div');
     subLi.innerText = subValue;
-    pageDisplay.appendChild(subLi);
+    newContaner.appendChild(subLi);
+
   }
+  pageDisplay.append(newContaner);
+}
+
+
--- logic.js
+++ logic.js
@@ -1,9 +1,19 @@
 let count = 1;
 
+const storedData = localStorage.getItem("selectedList");
+
+let fullCardSetCount = JSON.parse(storedData).length;
+
 function nextOnClick(){
+    if(count > fullCardSetCount-1){
+        count=fullCardSetCount;
+    }else{
     count++;
+    }
+
     let counter = document.querySelector(".counter");
-    counter.innerHTML = count + "/4";
+    counter.innerHTML = count + "/" + fullCardSetCount;
+    updateCard(storedData)
 }
 
 function prevOnClick(){
@@ -13,11 +23,65 @@ function prevOnClick(){
         count--;
     }
     let counter = document.querySelector(".counter");
-    counter.innerHTML = count + "/4";
+    counter.innerHTML = count +"/" + fullCardSetCount;
+    updateCard(storedData)
+};
   
+
+function rotateDiv(classType) {
+    const div = document.querySelector('.card');
+    div.classList.toggle(classType);
+
 }
 
-function rotateDiv() {
-    const div = document.querySelector('.card');
-    div.classList.toggle('cardRotated');
+
+function updateCard(storedData){
+
+    if (storedData) {
+        const selectedList = JSON.parse(storedData);
+        const frontContentElement = document.querySelector(".cardContent.front");
+        const backContentElement = document.querySelector(".cardContent.back");
+      
+        frontContentElement.innerText = selectedList[count-1][0];
+        backContentElement.innerText = selectedList[count-1][1];
+    
+    } else {
+      console.log("Brak danych w localStorage");
+    }
+    
+}
+
+let goodCounter = 0;
+let badCounter = 0;
+
+function correctAnswer(){
+    if(checkAnswerSum(goodCounter,badCounter)){
+        console.log("Za duzo punktow");
+    }else{
+    goodCounter++;
+    const correctAnswerElement = document.querySelector(".correctScore");
+
+    correctAnswerElement.innerText = "Correct: " + goodCounter;
+
+    nextOnClick()
+}
+}
+
+function incorrectAnswer(){
+    if(checkAnswerSum(goodCounter,badCounter)){
+        console.log("Za duzo punktow");
+    }else{
+    badCounter++;
+    const badAnswerElement = document.querySelector(".incorrectScore");
+
+    badAnswerElement.innerText = "Incorrect: " + badCounter;
+    nextOnClick()
+    }
+}
+
+function checkAnswerSum(goodCounter,badCounter){
+    if(goodCounter + badCounter == fullCardSetCount){
+        return true;
+    }
+    return false;
 }
